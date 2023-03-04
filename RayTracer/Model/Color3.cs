087df2e465a8fb3f6602aaf6eedc4d50eb831cad
Three Strikes <1190982@isep.ipp.pt>
2023-03-04 12:15:37 +0000

    Criação da classe Image e remoção dos private setters.

--- RayTracer/Model/Color3.cs
+++ RayTracer/Model/Color3.cs
@@ -12,25 +12,42 @@ namespace RayTracer.Model
     internal class Color3
     {
         // O valor vermelho da cor.
-        private readonly double _red;
+
+        public double Red { get; set; }
+
         // O valor verde da cor.
-        private readonly double _green;
+        public double Green { get; set; }
+
         // O valor azul da cor.
-        private readonly double _blue;
+        public double Blue { get; set; }
 
         /// <summary>
         /// O construtor da classe. Leva como parâmetros os valores das componentes da cor, entre 0.0 e 1.0.
-        /// A verificação é feita logo ao ser chamado o construtor.
+        /// As verificações são feitas quando o construtor é chamado.
         /// </summary>
         /// <param name="red">O valor vermelho da cor.</param>
         /// <param name="green">O valor verde da cor.</param>
         /// <param name="blue">O valor azul da cor.</param>
         public Color3(double red, double green, double blue){
-            if ((red < 0.0 || red > 1.0) && (green < 0.0 || green > 1.0) && (blue < 0.0 || blue > 1.0))
-            {
 
+            if (red < 0.0 || red > 1.0)
+            {
+                throw new ArgumentOutOfRangeException("red","The red value is not between 0 and 1!");
             }
 
+            if (green < 0.0 || green > 1.0)
+            {
+                throw new ArgumentOutOfRangeException("green", "The green value is not between 0 and 1!");
+            }
+
+            if(blue < 0.0 || blue > 1.0)
+            {
+                throw new ArgumentOutOfRangeException("blue", "The blue value is not between 0 and 1!");
+            }
+
+            Red = red;
+            Green = green;
+            Blue = blue;
         }
     }
 }
---
+++ RayTracer/Model/Image.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RayTracer.Model
+{
+    /// <summary>
+    /// Classe que representa a Imagem a gerar.
+    /// </summary>
+    internal class Image
+    {
+        /// <summary>
+        /// O comprimento da imagem em pixeis.
+        /// </summary>
+        public int Length { get; set; }
+        /// <summary>
+        /// A largura da imagem em pixeis.
+        /// </summary>
+        public int Width { get; set; }
+        /// <summary>
+        /// A cor de fundo da imagem.
+        /// </summary>
+        public Color3 BackgroundColor { get; set; }
+
+        public Image(int length, int width, Color3 backgroundColor) {
+            if (length < 0) { 
+                throw new ArgumentOutOfRangeException("length", "The length cannot be below zero!");
+            }
+            if (width < 0)
+            {
+                throw new ArgumentOutOfRangeException("length", "The width cannot be below zero!");
+            }
+
+            Length = length;
+            Width = width;
+            BackgroundColor = backgroundColor;
+        }
+    }
+}
