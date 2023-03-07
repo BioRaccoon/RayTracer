caa3f2b1cf3fbdd6753fe7bb944b1412d7afbc13
Three Strikes <1190982@isep.ipp.pt>
2023-03-07 19:25:59 +0000

    Created the Hit, Material and Ray classes.

---
+++ RayTracer/Model/Hit.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RayTracer.Model
+{
+    internal class Hit
+    {
+        bool Found { get; set; }
+        Material MaterialHit { get; set; }
+        Vector3 IntersectionPoint { get; set; }
+        Vector3 NormalVector { get; set; }
+        double TotalDistance { get; set; }
+        double FoundDistance { get; set; }
+
+        public Hit(bool foundHit, Material materialHit, Vector3 intersectionPoint, Vector3 normal, double distance, double foundDistance) {
+            Found = foundHit;
+            MaterialHit = materialHit;
+            IntersectionPoint = intersectionPoint;
+            NormalVector = normal;
+            TotalDistance = distance;
+            FoundDistance = foundDistance;
+        }
+
+    }
+}
---
+++ RayTracer/Model/Ray.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RayTracer.Model
+{
+    /// <summary>
+    /// Classe que representa um raio de luz no espaço.
+    /// </summary>
+    internal class Ray
+    {
+        /// <summary>
+        /// A origem do raio de luz.
+        /// </summary>
+        public Vector3 Origin { get; set; }
+        /// <summary>
+        /// A direção do raio de luz.
+        /// </summary>
+        public Vector3 Direction { get; set; }
+        /// <summary>
+        /// A distância total do raio de luz à superfície alvo.
+        /// </summary>
+        //public int TotalDistance { get; set; }
+
+        public Ray(Vector3 Origin, Vector3 Direction/*, int TotalDistance*/)
+        {
+            this.Origin = Origin;
+            this.Direction = Direction;
+            //this.TotalDistance = TotalDistance; //TODO: Não sei se isto é um parâmetro, mas é melhor perguntar
+        }
+
+        public Vector3 getPointInLine(double totalDistance)
+        {
+            return Origin.Add(Direction.ScalarMultiplication(totalDistance));
+        }
+    }
+}
