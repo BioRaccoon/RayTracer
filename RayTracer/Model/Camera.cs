e82404c0c2ae7996f1323c6ab41be3cc412779f4
Three Strikes <1190982@isep.ipp.pt>
2023-03-09 21:05:18 +0000

    Implemented the Camera and Transformation classes.

---
+++ RayTracer/Model/Camera.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RayTracer.Model
+{
+    internal class Camera
+    {
+        public int TransformationIndex { get; set; }
+        public double Distance { get; set; }
+        public double FieldOfView { get; set; }
+
+        public Camera(int cameraTransformationIndex, double cameraDistance, double fov)
+        {
+            TransformationIndex = cameraTransformationIndex;
+            Distance = cameraDistance;
+            FieldOfView = fov;  
+        }
+    }
+}
