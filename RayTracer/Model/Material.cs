e46413f0aa752c573436ba408a989c2b63122253
Three Strikes <1190982@isep.ipp.pt>
2023-03-09 21:01:14 +0000

    Material class implemented.

--- RayTracer/Model/Material.cs
+++ RayTracer/Model/Material.cs
@@ -8,5 +8,22 @@ namespace RayTracer.Model
 {
     internal class Material
     {
+        public Color3 Color { get; set; }
+        public double AmbientLight { get; set; }
+        public double DifuseLight { get; set; }
+        public double SpecularLight { get; set; }
+        public double Refraction { get; set; }
+        public double RefractionIndex { get; set; }
+
+        public Material(Color3 color, double ambient, double difuse, double specular, double refraction, double refractionIndex)
+        {
+            Color = color;
+            AmbientLight = ambient;
+            DifuseLight = difuse;
+            SpecularLight = specular;
+            Refraction = refraction;
+            RefractionIndex = refractionIndex;
+        }
+
     }
 }
