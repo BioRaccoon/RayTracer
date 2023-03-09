745477b12da125394ea4d9073bf907ff41df287e
Three Strikes <1190982@isep.ipp.pt>
2023-03-09 21:10:01 +0000

    Implemented the LightSource class.

---
+++ RayTracer/Model/LightSource.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RayTracer.Model
+{
+    internal class LightSource
+    {
+        public int TransformationIndex { get; set; }
+        public Color3 Intensity { get; set; }
+
+        public LightSource(int LightTransformationIndex, Color3 lightIntensity)
+        {
+            TransformationIndex = LightTransformationIndex;
+            Intensity = lightIntensity;
+        }
+    }
+}
